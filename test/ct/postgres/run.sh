72b0f97affda09d77b1f06a67d10775cd34ee0bc
Devon Bautista <devonb@lanl.gov>
2023-10-17 16:32:35 -0600

    Add integration tests for MAC addresses

--- test/ct/postgres/run.sh
+++ test/ct/postgres/run.sh
@@ -5,3 +5,6 @@ set -e
 
 # Test add/delete/get boot config by XName.
 /usr/bin/hurl --test "$(dirname $0)"/tests/xname/*.hurl
+
+# Test add/delete/get boot config by MAC address.
+/usr/bin/hurl --test "$(dirname $0)"/tests/mac/*.hurl
