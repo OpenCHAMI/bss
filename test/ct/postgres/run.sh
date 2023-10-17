4c8f3d1e13f370d48fc15d565d4ea9957a517f58
Devon Bautista <devonb@lanl.gov>
2023-10-17 16:38:42 -0600

    Add some output headings to integration test script

--- test/ct/postgres/run.sh
+++ test/ct/postgres/run.sh
@@ -4,7 +4,17 @@
 set -e
 
 # Test add/delete/get boot config by XName.
+echo '============================='
+echo 'EXECUTING xname UNIT TESTS...'
+echo '============================='
 /usr/bin/hurl --test "$(dirname $0)"/tests/xname/*.hurl
 
 # Test add/delete/get boot config by MAC address.
+echo '============================='
+echo 'EXECUTING mac UNIT TESTS...'
+echo '============================='
 /usr/bin/hurl --test "$(dirname $0)"/tests/mac/*.hurl
+
+echo '============================='
+echo 'ALL UNIT TESTS COMPLETED'
+echo '============================='
