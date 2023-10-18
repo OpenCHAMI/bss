202e081b3d2fc203fa2f4f92f1fa4e0ca0a9db17
Devon Bautista <devonb@lanl.gov>
2023-10-18 09:53:20 -0600

    Add integration tests for NIDs
    This test is currently commented out and will not run because of SMD
    behavior that would cause it to fail. The functionality of this test
    remains present, albeit commented, until this is resolved.

--- test/ct/postgres/run.sh
+++ test/ct/postgres/run.sh
@@ -15,6 +15,17 @@ echo 'EXECUTING mac UNIT TESTS...'
 echo '============================='
 /usr/bin/hurl --test "$(dirname $0)"/tests/mac/*.hurl
 
+# The version of SMD we are using does not support updating/displaying
+# NIDs, even though BSS can generate a boot script by NID. Since BSS
+# checks SMD to see if a node exists via its NID for this test, it will
+# fail. Therefore, this test is commented out for now until SMD handles
+# NIDs correctly.
+## Test add/delete/get boot config by NID.
+#echo '============================='
+#echo 'EXECUTING mac UNIT TESTS...'
+#echo '============================='
+#/usr/bin/hurl --test "$(dirname $0)"/tests/nid/*.hurl
+
 echo '============================='
 echo 'ALL UNIT TESTS COMPLETED'
 echo '============================='
