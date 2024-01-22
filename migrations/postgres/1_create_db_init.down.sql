ca9c62b284bac7cb5e9adb8e2dafee28fa20d843
Devon Bautista <devonb@lanl.gov>
2024-01-22 14:39:55 -0700

    Add license to postgres migration files

--- migrations/postgres/1_create_db_init.down.sql
+++ migrations/postgres/1_create_db_init.down.sql
@@ -1,23 +1,13 @@
--- MIT License
+-- Copyright © 2023 Triad National Security, LLC. All rights reserved.
 --
--- (C) Copyright [2019-2021] Hewlett Packard Enterprise Development LP
---
--- Permission is hereby granted, free of charge, to any person obtaining a
--- copy of this software and associated documentation files (the "Software"),
--- to deal in the Software without restriction, including without limitation
--- the rights to use, copy, modify, merge, publish, distribute, sublicense,
--- and/or sell copies of the Software, and to permit persons to whom the
--- Software is furnished to do so, subject to the following conditions:
---
--- The above copyright notice and this permission notice shall be included
--- in all copies or substantial portions of the Software.
---
--- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
--- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
--- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
--- THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
--- OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
--- ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
--- OTHER DEALINGS IN THE SOFTWARE.
+-- This program was produced under U.S. Government contract 89233218CNA000001 for
+-- Los Alamos National Laboratory (LANL), which is operated by Triad National
+-- Security, LLC for the U.S. Department of Energy/National Nuclear Security
+-- Administration. All rights in the program are reserved by Triad National
+-- Security, LLC, and the U.S. Department of Energy/National Nuclear Security
+-- Administration. The Government is granted for itself and others acting on its
+-- behalf a nonexclusive, paid-up, irrevocable worldwide license in this material
+-- to reproduce, prepare derivative works, distribute copies to the public,
+-- perform publicly and display publicly, and to permit others to do so.
 
 -- DROP SCHEMA IF EXISTS bssdb;
