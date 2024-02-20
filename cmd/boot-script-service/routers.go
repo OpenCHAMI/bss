d510cb4e4b8c627baa0d830bb6935d674965b64d
David J. Allen <allend@lanl.gov>
2024-02-20 15:59:34 -0700

    Split `serviceStatusAPI` into multiple functions with multiple routes

--- cmd/boot-script-service/routers.go
+++ cmd/boot-script-service/routers.go
@@ -114,7 +114,10 @@ func initHandlers() *chi.Mux {
 	router.HandleFunc(baseEndpoint+"/bootscript", bootScript)
 	router.HandleFunc(baseEndpoint+"/hosts", hosts)
 	router.HandleFunc(baseEndpoint+"/dumpstate", dumpstate)
-	router.HandleFunc(baseEndpoint+"/service/", service)
+	router.HandleFunc(baseEndpoint+"/service/status", serviceStatusResponse)
+	router.HandleFunc(baseEndpoint+"/service/version", serviceVersionResponse)
+	router.HandleFunc(baseEndpoint+"/service/hsm", serviceHSMResponse)
+	router.HandleFunc(baseEndpoint+"/service/etcd", serviceETCDResponse)
 	// cloud-init
 	router.HandleFunc(metaDataRoute, metaDataGet)
 	router.HandleFunc(userDataRoute, userDataGet)
