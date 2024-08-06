793d11981791ede63886ddb741fa770da59d5bd4
Devon Bautista <devonb@lanl.gov>
2024-08-06 13:10:05 -0600

    postgres: Add LogEndpointAccess() function

--- internal/postgres/endpoints.go
+++ internal/postgres/endpoints.go
@@ -13,8 +13,53 @@
 
 package postgres
 
+import (
+	"fmt"
+	"time"
+
+	"github.com/OpenCHAMI/bss/pkg/bssTypes"
+)
+
 type EndpointAccess struct {
 	Name      string `json:"name"`
 	Endpoint  string `json:"endpoint"`
 	LastEpoch int64  `json:"last_epoch"`
+}
+
+// LogEndpointAccess takes a name and an endpoint type and adds a table entry to
+// the endpoint_accesses table with the current timestamp.
+func (bddb BootDataDatabase) LogEndpointAccess(name string, endpointType bssTypes.EndpointType) (err error) {
+	if name == "" {
+		err = fmt.Errorf("postgres.LogEndpointAccess: Argument 'name' cannot be empty")
+		return
+	}
+	if endpointType == "" {
+		err = fmt.Errorf("postgres.LogEndpointAccess: Argument 'endpointType' cannot be empty")
+		return
+	}
+
+	ts := time.Now()
+	ea := EndpointAccess{
+		Name: name,
+		Endpoint: string(endpointType),
+		LastEpoch: ts.Unix(),
+	}
+
+	err = bddb.addEndpointAccess(ea)
+	if err != nil {
+		err = fmt.Errorf("postgres.LogEndpointAccess: %v", err)
+	}
+
+	return
+}
+
+func (bddb BootDataDatabase) addEndpointAccess(ea EndpointAccess) (err error) {
+	execStr := `INSERT INTO endpoint_access (name, endpoint, last_epoch) VALUES ($1, $2, $3);`
+	_, err = bddb.DB.Exec(execStr, ea.Name, ea.Endpoint, ea.LastEpoch)
+	if err != nil {
+		err = fmt.Errorf("Error executing query to add endpoint access %v: %v", ea, err)
+		return
+	}
+
+	return
 }
