1668a26a13e82f477b8d05a1dba400076c087559
Devon Bautista <devonb@lanl.gov>
2024-08-06 12:15:05 -0600

    Add SQL migrations for endpoint_access table

---
+++ migrations/postgres/3_create_version2.up.sql
@@ -0,0 +1,24 @@
+-- Copyright © 2024 Triad National Security, LLC. All rights reserved.
+--
+-- This program was produced under U.S. Government contract 89233218CNA000001 for
+-- Los Alamos National Laboratory (LANL), which is operated by Triad National
+-- Security, LLC for the U.S. Department of Energy/National Nuclear Security
+-- Administration. All rights in the program are reserved by Triad National
+-- Security, LLC, and the U.S. Department of Energy/National Nuclear Security
+-- Administration. The Government is granted for itself and others acting on its
+-- behalf a nonexclusive, paid-up, irrevocable worldwide license in this material
+-- to reproduce, prepare derivative works, distribute copies to the public,
+-- perform publicly and display publicly, and to permit others to do so.
+
+BEGIN;
+
+--
+-- endpoint_access - Endpoint access history
+--
+CREATE TABLE IF NOT EXISTS endpoint_access (
+	name varchar,
+	endpoint varchar,
+	last_epoch int
+);
+
+COMMIT;
