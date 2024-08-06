a36adbfaa349d21ed38a23c36a9d2a30f05974cf
Devon Bautista <devonb@lanl.gov>
2024-08-06 14:33:26 -0600

    postgres: Add SearchEndpointAccesses() function

--- internal/postgres/endpoints.go
+++ internal/postgres/endpoints.go
@@ -14,6 +14,7 @@
 package postgres
 
 import (
+	"database/sql"
 	"fmt"
 	"time"
 
@@ -24,6 +25,65 @@ type EndpointAccess struct {
 	Name      string `json:"name"`
 	Endpoint  string `json:"endpoint"`
 	LastEpoch int64  `json:"last_epoch"`
+}
+
+// SearchEndpointAccesses takes the name of a node (xname) and a BSS endpoint as
+// arguments and returns a slice of EndpointAccess structs representing
+// timestamps of when the passed endpoint was accessed for the passed name. If
+// endpointType is empty, then all endpoint accesses for the given name are
+// returned. If name is empty, then all accesses for the given endpoint are
+// returned. If both arguments are empty, then all endpoint accesses for all
+// names are returned.
+func (bddb BootDataDatabase) SearchEndpointAccesses(name string, endpointType bssTypes.EndpointType) (accesses []bssTypes.EndpointAccess, err error) {
+	qstr := `SELECT * FROM endpoint_access`
+
+	// Only construct query with WHERE clause if both arguments are NOT
+	// empty.
+	if name != "" || endpointType != "" {
+		qstr += ` WHERE`
+		strs := []string{name, string(endpointType)}
+		for first, i := true, 0; i < len(strs); i++ {
+			if strs[i] != "" {
+				if !first {
+					qstr += ` AND`
+				}
+				switch i {
+				case 0:
+					qstr += fmt.Sprintf(` name = '%s'`, strs[0])
+				case 1:
+					qstr += fmt.Sprintf(` endpoint = '%s'`, strs[1])
+				}
+				first = false
+			}
+		}
+	}
+	qstr += `;`
+	var rows *sql.Rows
+	rows, err = bddb.DB.Query(qstr)
+	if err != nil {
+		err = fmt.Errorf("postgres.SearchEndpointAccesses: Could not query endpoint access table in boot database: %v", err)
+		return
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var (
+			ea bssTypes.EndpointAccess
+		)
+		err = rows.Scan(&ea.Name, &ea.Endpoint, &ea.LastEpoch)
+		if err != nil {
+			err = fmt.Errorf("postgres.SearchEndpointAccesses: Could not scan results into EndpointAccess: %v", err)
+			return
+		}
+		accesses = append(accesses, ea)
+	}
+	// Did a rows.Next() return an error?
+	if err = rows.Err(); err != nil {
+		err = fmt.Errorf("postgres.SearchEndpointAccesses: Could not parse query results: %v", err)
+		return
+	}
+
+	return
 }
 
 // LogEndpointAccess takes a name and an endpoint type and adds a table entry to
