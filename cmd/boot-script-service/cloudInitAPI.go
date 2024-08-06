d6f0d290a26874f8608a53cca3b970eec5f8a397
Devon Bautista <devonb@lanl.gov>
2024-08-06 15:07:19 -0600

    Add postgres support for endpointHistoryGetAPI()

--- cmd/boot-script-service/cloudInitAPI.go
+++ cmd/boot-script-service/cloudInitAPI.go
@@ -293,9 +293,17 @@ func endpointHistoryGetAPI(w http.ResponseWriter, r *http.Request) {
 		lastAccessTypeStruct = bssTypes.EndpointType(endpoint)
 	}
 
-	accesses, err := SearchEndpointAccessed(name, lastAccessTypeStruct)
+	var (
+		accesses []bssTypes.EndpointAccess
+		err      error
+	)
+	if useSQL {
+		accesses, err = bssdb.SearchEndpointAccesses(name, bssTypes.EndpointType(endpoint))
+	} else {
+		accesses, err = SearchEndpointAccessed(name, lastAccessTypeStruct)
+	}
 	if err != nil {
-		errMsg := fmt.Sprintf("Failed to search for name: %s, endpoint: %s", name, endpoint)
+		errMsg := fmt.Sprintf("Failed to search for name=%q, endpoint=%q: %v", name, endpoint, err)
 		base.SendProblemDetailsGeneric(w, http.StatusInternalServerError, errMsg)
 		log.Printf("BSS request failed: %s", errMsg)
 		return
